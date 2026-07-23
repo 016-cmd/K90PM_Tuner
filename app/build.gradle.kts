b16b98767dfdc669780226da58a1c62039b41348
016-cmd <lujiaming142536@gmail.com>
2026-07-23 17:50:04 +0800

    Xposed stubs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
