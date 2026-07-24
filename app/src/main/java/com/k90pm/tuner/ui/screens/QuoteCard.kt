41710777681b9ed729cc3c2904d392c03ba1ceeb
016-cmd <lujiaming142536@gmail.com>
2026-07-24 21:51:21 +0800

    Xposed stubs

--- app/src/main/java/com/k90pm/tuner/ui/screens/QuoteCard.kt
+++ app/src/main/java/com/k90pm/tuner/ui/screens/QuoteCard.kt
@@ -1,5 +1,6 @@
 package com.k90pm.tuner.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
