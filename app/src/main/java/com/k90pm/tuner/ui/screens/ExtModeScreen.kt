912386186f8ba4586f664ab46e48b0f8d687a275
016-cmd <lujiaming142536@gmail.com>
2026-07-25 00:12:10 +0800

    Xposed stubs

--- app/src/main/java/com/k90pm/tuner/ui/screens/ExtModeScreen.kt
+++ app/src/main/java/com/k90pm/tuner/ui/screens/ExtModeScreen.kt
@@ -8,6 +8,7 @@ import android.media.session.MediaSessionManager
 import android.media.session.PlaybackState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
